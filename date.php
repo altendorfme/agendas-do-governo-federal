0a0370c03893c57a835f58cdcd4a2d3a3545d97c
altendorfme <altendorfme@gmail.com>
2021-05-24 18:19:35 -0300

    force download specific date

---
+++ date.php
@@ -0,0 +1,37 @@
+<?php
+require_once(dirname(__FILE__).'/config.php');
+require_once(dirname(__FILE__).'/functions.php');
+
+$getopt = getopt('date:,schedule', ["date:","schedule:"]);
+if( !empty($getopt) ) {
+
+    if( (!empty($getopt['date'])) && (!empty($getopt['schedule'])) ) {
+        $date = $getopt['date'];
+        $schedule_id = $getopt['schedule'];
+        
+        echo '[Date]'.PHP_EOL;
+        echo 'Date: '.$date.PHP_EOL;
+        echo 'Schedule ID: '.$schedule_id.PHP_EOL;
+        
+        $mysqli = new mysqli($GLOBALS['mysql_host'], $GLOBALS['mysql_user'], $GLOBALS['mysql_password'], $GLOBALS['mysql_database']);
+        $mysqli->set_charset("utf8");
+        
+        $schedule = "SELECT * FROM `schedule` WHERE `id` = ".$schedule_id;
+        $schedule_query = mysqli_query($mysqli, $schedule);
+        while($data = mysqli_fetch_array($schedule_query)) {
+            $id = $data['id'];
+            $name = $data['name'];
+            $url = $data['url'];
+        
+            echo '==='.PHP_EOL;
+        
+            echo '['.$id.']: '.$name.PHP_EOL;
+            echo $url.PHP_EOL;
+        
+            get_events_by_date($date, $id, $url);
+        }
+
+    }
+
+}
+
