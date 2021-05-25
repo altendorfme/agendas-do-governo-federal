f51db0a08b8137b17074250f0f734b690cacc64e
altendorfme <altendorfme@gmail.com>
2021-05-24 23:38:28 -0300

    endpoint generator

---
+++ index.php
@@ -0,0 +1,114 @@
+
+<?php
+require_once(dirname(__FILE__).'/config.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agendas do Governo Federal</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="//fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background: #FBFBFB;
+            font-family: 'Source Code Pro', monospace;
+            color: #989fb1;
+            padding: 1rem;
+            font-size: 16px;
+        }
+        a {
+            color: #989fb1;
+        }
+        a:hover {
+            text-decoration: none;
+        }
+        h1 {
+            margin: 0;
+            padding-bottom: .4rem;
+            color: #4876d6;
+        }
+        select {
+            font-family: 'Source Code Pro', monospace;
+            font-size: 16px;
+            padding: .2rem;
+            margin-bottom: .5rem;
+        }
+        .endpoint {
+            display: none;
+        }
+        .endpoint p {
+            margin: 0;
+        }
+        .endpoint a {
+            color: #0c969b;
+        }
+    </style>
+</head>
+<body>
+    <h1>Agendas do Governo Federal</h1>
+
+    <select name="schedule" id="schedule">
+        <option value="" selected disabled>Agendas</option>
+        <?php
+            $mysqli = new mysqli($GLOBALS['mysql_host'], $GLOBALS['mysql_user'], $GLOBALS['mysql_password'], $GLOBALS['mysql_database']);
+            $mysqli->set_charset("utf8");
+
+            $schedules = "SELECT * FROM `schedule`";
+            $schedules_query = mysqli_query($mysqli, $schedules);
+            
+            while($data = mysqli_fetch_array($schedules_query)) {
+                $id = $data['id'];
+                $name = $data['name'];
+                $url = $data['url'];
+                $active = $data['active'];
+                ?>
+                <option value="<?php echo $id; ?>" data-url="<?php echo $url; ?>" <?php ($active == 0) ? 'disabled' : ''; ?>><?php echo $name; ?></option>
+                <?php
+            }
+        ?>
+    </select>
+
+    <select name="year" id="year">
+        <option value="" selected disabled>Ano</option>
+        <option value="">Todos</option>
+        <option value="2019">2019</option>
+        <option value="2020">2020</option>
+        <option value="2021">2021</option>
+    </select>
+
+    <div class="endpoint" id="endpoint">
+        <p>CSV: <a href="#" target="_blank" id="csv">/api/?schedule=1&year=2021&format=csv</a></p>
+        <p>JSON: <a href="#" target="_blank" id="json">/api/?schedule=1&year=2021&format=json</a></p>
+        <p>Fonte: <a href="#" target="_blank" id="source">gov.br</a></a>
+    </div>
+
+    <p><a href="https://github.com/altendorfme/agendas-do-governo-federal" target="_blank"><strong>Github</strong></a> / <a href="https://twitter.com/altendorfme" target="_blank"><strong>Twitter</strong></a></p>
+
+    <script src="//cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> 
+    <script>
+        $(function() {
+            $('#schedule,#year').on('change',function(){
+                var id = $('#schedule').val();
+                var url = $('#schedule').find(':selected').attr('data-url');
+                var year = $('#year').val();
+                $('#endpoint').show();
+                $('#source').attr('href', url);
+                if(year == null || year == '') {
+                    var csv = '/api/?schedule='+id+'&format=csv';
+                    var json = '/api/?schedule='+id+'&format=json';
+                    $('#csv').attr('href', csv).html(csv);
+                    $('#json').attr('href', json).html(json);
+                } else {
+                    var csv = '/api/?schedule='+id+'&year='+year+'&format=csv';
+                    var json = '/api/?schedule='+id+'&year='+year+'&format=json';
+                    $('#csv').attr('href', csv).html(csv);
+                    $('#json').attr('href', json).html(json);
+                }
+            })
+        });
+    </script>
+</body>
+</html>
