65e7ab11d96390d747ad5417dd645a470816b283
altendorfme <altendorfme@gmail.com>
2021-05-24 23:51:58 -0300

    add favicons and social meta tags

--- index.php
+++ index.php
@@ -9,6 +9,35 @@ require_once(dirname(__FILE__).'/config.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendas do Governo Federal</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+    <link rel="manifest" href="/assets/site.webmanifest">
+    <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/assets/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="/assets/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta name="description" content="Agendas do Governo Federal">
+    <meta name="image" content="https://agendas-do-governo-federal.bot.nu/ogimage.png">
+    <meta itemprop="name" content="Agendas do Governo Federal">
+    <meta itemprop="description" content="Agendas do Governo Federal">
+    <meta itemprop="image" content="https://agendas-do-governo-federal.bot.nu/ogimage.png">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Agendas do Governo Federal">
+    <meta name="twitter:description" content="Agendas do Governo Federal">
+    <meta name="twitter:site" content="@altendorfme">
+    <meta name="twitter:image:src" content="https://agendas-do-governo-federal.bot.nu/ogimage.png">
+    <meta name="og:title" content="Agendas do Governo Federal">
+    <meta name="og:description" content="Agendas do Governo Federal">
+    <meta name="og:image" content="https://agendas-do-governo-federal.bot.nu/ogimage.png">
+    <meta name="og:url" content="https://agendas-do-governo-federal.bot.nu">
+    <meta name="og:site_name" content="Agendas do Governo Federal">
+    <meta name="og:locale" content="pt_BR">
+    <meta name="og:type" content="website">
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="//fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;700&display=swap" rel="stylesheet">
     <style>
